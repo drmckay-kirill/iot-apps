704112b68cb39ce1a6075a091993498911910d01
drmckay-kirill <drmckay-kirill@yandex.ru>
2017-12-29 23:14:33 +0500

    fiware: mqtt -> iot agent -> broker in docker

---
+++ fiware/iotagent/config.js
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2015 Telefonica Investigación y Desarrollo, S.A.U
+ *
+ * This file is part of iotagent-json
+ *
+ * iotagent-json is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * iotagent-json is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with iotagent-json.
+ * If not, seehttp://www.gnu.org/licenses/.
+ *
+ * For those usages not covered by the GNU Affero General Public License
+ * please contact with::[contacto@tid.es]
+ */
+var config = {};
+
+config.mqtt = {
+    host: 'mosquitto',
+    defaultKey: '1234',
+    port: 1883,
+    thinkingThingsPlugin: true
+};
+
+config.iota = {
+    logLevel: 'DEBUG',
+    timestamp: true,
+    contextBroker: {
+        host: 'orion',
+        port: '1026'
+    },
+    server: {
+        port: 4041
+    },
+    deviceRegistry: {
+        type: 'mongodb'
+    },
+    mongodb: {
+        host: 'mongo',
+        port: '27017',
+        db: 'iotagentjson'
+    },
+    types: {},
+    service: 'myHome',
+    subservice: '/environment',
+    providerUrl: 'http://172.19.0.5:4041',
+    deviceRegistrationDuration: 'P1M',
+    defaultType: 'Thing',
+    defaultKey: '1234'
+};
+
+config.configRetrieval = false;
+
+module.exports = config;
